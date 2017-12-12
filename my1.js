f8bd24d43ed3cf3246bf6b8b98973ce3fe83ff7c
chajs226 <chajs226@gmail.com>
2017-12-12 18:33:06 +0900

    js 추가
    js 추가

---
+++ my1.js
@@ -0,0 +1,31 @@
+$(function() {
+
+
+// configuration
+
+// button
+var add = $('#add');
+
+// list container
+var listContainer = $('#list');
+
+
+
+// click event for button
+
+add.on('click', function() {
+
+     event.preventDefault(); // stop default behaviour of submit button
+    // value of input
+    inputValue = $('#input').val();
+
+    // add new list item
+    listContainer.prepend('<li> ' + inputValue + '</li>');
+    // clear value input
+    $('#input').val('');
+
+
+
+});
+
+});
