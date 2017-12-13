715f593a966ce469571f2076a01a1c027c221bc1
chajs226 <chajs226@gmail.com>
2017-12-13 17:52:22 +0900

    js 약간수정 - alert 창 띄움
    js 약간수정 - alert 창 띄움

--- my1.js
+++ my1.js
@@ -1,31 +1,39 @@
-$(function() {
+var save = document.getElementById('btnSave');
 
-
-// configuration
-
-// button
-var add = $('#add');
-
-// list container
-var listContainer = $('#list');
+btnSave.addEventListener('click', function() {
+  alert('aaaaa');
+})
 
 
 
-// click event for button
-
-add.on('click', function() {
-
-     event.preventDefault(); // stop default behaviour of submit button
-    // value of input
-    inputValue = $('#input').val();
-
-    // add new list item
-    listContainer.prepend('<li> ' + inputValue + '</li>');
-    // clear value input
-    $('#input').val('');
-
-
-
-});
-
-});
+// $(function() {
+//
+//
+// // configuration
+//
+// // button
+// var add = $('#add');
+//
+// // list container
+// var listContainer = $('#list');
+//
+//
+//
+// // click event for button
+//
+// add.on('click', function() {
+//
+//      event.preventDefault(); // stop default behaviour of submit button
+//     // value of input
+//     inputValue = $('#input').val();
+//
+//     // add new list item
+//     listContainer.prepend('<li> ' + inputValue + '</li>');
+//     // clear value input
+//     $('#input').val('');
+//
+//
+//
+// });
+//
+// });
