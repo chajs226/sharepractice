786fccf9de6460a1e16419e363f2578c0667b3e8
chajs226 <chajs226@gmail.com>
2017-12-25 22:56:22 +0900

    php modify
    php modify

--- todo.php
+++ todo.php
@@ -6,8 +6,8 @@ $result = mysqli_query($conn, "SELECT * FROM todo");
 
 $row = mysqli_fetch_assoc($result);
 
-// echo $row['todo_title'];
-// echo $row['todo_desc'];
+ echo $row['todo_title'];
+ echo $row['todo_desc'];
  ?>
 
 
