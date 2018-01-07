f9073dc1a269d56959ab9d5b134d5dd9af4f0f20
chajs226 <chajs226@gmail.com>
2018-01-07 17:11:29 +0900

    ajax
    ajax 리턴값이 자꾸 fail임..
    dB에 인서트는 됨.

--- signup.js
+++ signup.js
@@ -7,27 +7,29 @@ btnSignUp.addEventListener('click', function() {
       $('#inputPassword').val() == '' ||
       $('#inputConfirmPassword').val() == '' ) {
     alert("입력 정보를 다시 확인바랍니다.");
+    window.location = "index.php?id=signup";
+  }
+  else if ($('#inputPassword').val() != $('#inputConfirmPassword').val())  {
+    alert("비밀번호가 틀렸습니다. 입력 정보를 다시 확인바랍니다.");
+    window.location = "index.php?id=signup";
   }
   else {
     $.ajax({
-    type: "POST", // POST형식으로 폼 전송
-    url: "./signup.php", // 목적지
-    timeout: 10000,
+    type: 'POST', // POST형식으로 폼 전송
+    dataType: 'json',
+    url: './signup.php', // 목적지
     data: {
       "email": $('#inputEmail').val(),
       "nicname": $('#inputNickname').val(),
       "password": $('#inputPassword').val()
-    },
-    cache: false,
-    dataType: "text",
-    success:function(args){
-      // $('#resultComment').html("환영합니다.");
-      alert("환영합니다.");
-    },
-    error: function(request,status,error) { // 전송 실패
-      alert("code:"+request.status+"\n"+"message:"+request.responseText+"\n"+"error:"+error);
+      },
+    success: function (data) {
+        console.log(data);
+        $('#response').html(data);
+      },
+    error: function (request, status, error) {
+        console.log('code: '+request.status+"\n"+'message: '+request.responseText+"\n"+'error: '+error);
+      }
+    });
   }
-});
-}
-
 });
--- signup.php
+++ signup.php
@@ -1,5 +1,5 @@
 <?php
-ini_set( "display_errors", 1 );
+// ini_set( "display_errors", 1 );
 include("config.php");
 
 $data_stream = "'".$_POST['email']."','".$_POST['nickname']."','".$_POST['password']."'";
@@ -8,7 +8,8 @@ $query = "insert into userinfo(email,nickname,password) values (".$data_stream."
 $result = mysqli_query($conn, $query);
 
 mysqli_close($connect);
-echo ('가입이 완료되었습니다. 메인 화면으로 이동합니다..');
+echo "가입이 완료되었습니다. 로그인 화면으로 이동합니다..";
+// echo("<meta http-equiv='Refresh' content='2; URL=index.php?id=signin'>");
 
 // $result = mysqli_query($conn, "INSERT INTO USERINFO(email, nickname, password, vald, usdt, updt)
 //                                 values($_POST['email'], $_POST['nickname'], $_POST['password'], 'Y', NOW(), NOW())");
