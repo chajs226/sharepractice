4d017ed52311e0a609e95103bcd8b885c7d148a9
chajs226 <chajs226@gmail.com>
2018-01-14 09:46:03 +0900

    로그인
    자꾸 select 리턴값이 null이 나옴

--- signin.php
+++ signin.php
@@ -2,25 +2,27 @@
 // ini_set( "display_errors", 1 );
 include("config.php");
 
-// $query = "select email, nickname
-//           from userinfo
-//           where email = '".$_POST['email']."'"
-//           "and password = '".$_POST['password']."'";
+$query = "select email, nickname from userinfo where vald = 'Y' and email = '".$_POST['email']."' and password = '".$_POST['password']."'";
 
-          $query = "select email, nickname
-                    from userinfo
-                    where email = '99@99'
-                    and password = '9'";
+//$query = "select email, nickname from userinfo where vald = 'Y' and email = '99@99' and password = '9'";
 // insert into ..... values ('.$_POST['email'].','.$_POST['nickname'].','.$_POST['password'].',''
 $result = mysqli_query($conn, $query);
 
-if ($result) {
-    $return = array ('result'=>true, 'message'=>"로그인 되었습니다.");
+error_log(var_export($result, 1));
+
+$count = mysqli_num_rows($result);
+error_log(var_export($count, 1));
+
+if ($count == 1) {
     $row = mysql_fetch_row($return);
+    error_log(var_export($row, 1));
+
     session_start();
     $_SESSION['email'] = $row[0];
     $_SESSION['nickname'] = $row[1];
+    $return = array ('result'=>true, 'message'=>"로그인 되었습니다.");
     echo json_encode($return);
+
   } else {
     $return = array ('result'=>false, 'message'=>"계정 정보가 올바르지 않습니다.");
     // echo "가입이 완료되었습니다. 로그인 화면으로 이동합니다..";
