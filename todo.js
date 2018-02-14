7340aba07ff7acbf06cf674821b7da1683fdf194
chajs226 <chajs226@gmail.com>
2018-02-14 17:46:13 +0900

    연습추가 에러
    '연습추가'를 했는데,, 어째서 index.php 화면으로 가는걸까;;;;
    확인필요...

--- todo.js
+++ todo.js
@@ -20,12 +20,31 @@ window.onload = function() {
       alert("전송에 실패했습니다.");
     }
   });
+
+  //리스트 그룹에 노드 추가
+  var aNode = document.createElement("a");
+  aNode.className = "list-group-item list-group-item-action"
+  aNode.href = "#"
+  //제목
+  var h4Node = document.createElement("h4");
+  h4Node.className = "list-group-item-heading"
+  var h4text = document.createTextNode($('#recipient-name').val());
+  h4Node.appendChild(h4text);
+  //내용
+  var pNode = document.createElement("p");
+  pNode.className = "list-group-item-text"
+  var ptext = document.createTextNode($('#message-text').val());
+  pNode.appendChild(ptext);
+
+  aNode.appendChild(h4Node);
+  aNode.appendChild(pNode);
+
+  document.getElementById("todoList").appendChild(aNode);
 }
 
 
 
 var save = document.getElementById('btnSave');
-
 btnSave.addEventListener('click', function() {
 
  if ($('#todo_title').val() == '') {
